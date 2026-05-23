3cf8e655cba7e13dae0eb8b66e9667ff3a8885b0
ValentinaSchaab <valentischaab@gmail.com>
2026-05-23 17:21:11 -0300

    agrega diccionario Explorer

---
+++ DiccionariosEnBruto/Explorer/Explorer.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2026 El Equipo del Proyecto DAV
+# Universidad Autónoma de Entre Ríos (UADER)
+# Bajo la dirección de Guillermo Gerard y Gallo Fabricio David
+#
+# Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+# bajo los términos de la Licencia Pública General GNU tal como fue publicada
+# por la Fundación para el Software Libre, en la versión 3 de la Licencia.
+#
+# Este programa se distribuye con la esperanza de que sea útil,
+# pero SIN NINGUNA GARANTÍA; incluso sin la garantía implícita de
+# MERCANTIBILIDAD o APTITUD PARA UN PROPÓSITO PARTICULAR. Consulte la
+# Licencia Pública General GNU para más detalles.
+#
+# Deberías haber recibido una copia de la Licencia Pública General GNU
+# junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
+
+from Archivos.Archivos       import archivos
+from Ventanas.Ventanas       import ventanas
+from Intercambio.Intercambio import intercambio
+from Herramientas.Herramientas import herramientas
+
+explorer = {}
+explorer.update(archivos)
+explorer.update(ventanas)
+explorer.update(intercambio)
+explorer.update(herramientas)
