b41ca638041b10dddcda509c6693b2e202f1d654
Joaquin Poggio <joaquinpoggio04@gmail.com>
2026-05-23 21:29:58 -0300

    Agrego Diccionarios PartWorkBench

--- DiccionariosEnBruto/PartWorkBench/ayuda.py
+++ DiccionariosEnBruto/PartWorkBench/ayuda.py
@@ -21,7 +21,23 @@ from .cube.ayuda     import ayuda as ayuda_cube
 from .cylinder.ayuda import ayuda as ayuda_cylinder
 from .ellipse.ayuda  import ayuda as ayuda_ellipse
 from .line.ayuda     import ayuda as ayuda_line
-
+from .new_sketch.ayuda import ayuda as ayuda_new_sketch
+from .part_chamfer.ayuda import ayuda as ayuda_part_chamfer
+from .part_color_per_face.ayuda import ayuda as ayuda_part_color_per_face
+from .part_cross_sections.ayuda import ayuda as ayuda_part_cross_sections
+from .part_extrude.ayuda import ayuda as ayuda_part_extrude
+from .part_fillet.ayuda import ayuda as ayuda_part_fillet
+from .part_loft.ayuda import ayuda as ayuda_part_loft
+from .part_makeface.ayuda import ayuda as ayuda_part_makeface
+from .part_mirror.ayuda import ayuda as ayuda_part_mirror
+from .part_offset.ayuda import ayuda as ayuda_part_offset
+from .part_offset2d.ayuda import ayuda as ayuda_part_offset2d
+from .part_projection_on_surface.ayuda import ayuda as ayuda_part_projection_on_surface
+from .part_revolve.ayuda import ayuda as ayuda_part_revolve
+from .part_ruled_surface.ayuda import ayuda as ayuda_part_ruled_surface
+from .part_scale.ayuda import ayuda as ayuda_part_scale
+from .part_section.ayuda import ayuda as ayuda_part_section
+from .part_sweep.ayuda import ayuda as ayuda_part_sweep
 
 def ayuda():
     ayuda_box()
@@ -37,3 +53,37 @@ def ayuda():
     ayuda_ellipse()
     print()
     ayuda_line()
+    print()
+    ayuda_new_sketch()
+    print()
+    ayuda_part_chamfer()
+    print()
+    ayuda_part_color_per_face()
+    print()
+    ayuda_part_cross_sections()
+    print()
+    ayuda_part_extrude()
+    print()
+    ayuda_part_fillet()
+    print()
+    ayuda_part_loft()
+    print()
+    ayuda_part_makeface()
+    print()
+    ayuda_part_mirror()
+    print()
+    ayuda_part_offset()
+    print()
+    ayuda_part_offset2d()
+    print()
+    ayuda_part_projection_on_surface()
+    print()
+    ayuda_part_revolve()
+    print()
+    ayuda_part_ruled_surface()
+    print()
+    ayuda_part_scale()
+    print()
+    ayuda_part_section()
+    print()
+    ayuda_part_sweep()
