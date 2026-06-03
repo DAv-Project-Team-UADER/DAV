665ada2bee8d8335f16521396905423be787f5c4
benjaklim <benjaklimczuk@gmail.com>
2026-06-03 19:09:47 -0300

    feat: Actualización completa de ComponentesDAV con dependencias unificadas

---
+++ ComponentesDAV/InterfazDAV/run_interfaz.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\HP\Desktop\pet dav freecad\DAV\ComponentesDAV\InterfazDAV"
+start "" "C:\Users\HP\Desktop\pet dav freecad\DAV\ComponentesDAV\IntegracionGUI\GUIFreeCad\.venv\Scripts\pythonw.exe" "C:\Users\HP\Desktop\pet dav freecad\DAV\ComponentesDAV\InterfazDAV\main.py"
