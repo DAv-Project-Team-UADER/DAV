e93bc762c75a5fd8a73515828a683aa463b033b9
Jesus Serra <164093027+jesusmaximo1211@users.noreply.github.com>
2026-06-09 19:19:13 -0300

    Fix copyright notice and add English mapping comments
    Signed-off-by: Jesus Serra <164093027+jesusmaximo1211@users.noreply.github.com>

--- DiccionariosEnBruto/Workbench/Sketcher/Geometry/circle/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/Sketcher/Geometry/circle/TraduceToEn.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2026 El Equipo del Proyecto DAV
-# Copyright (C) 2026 The DAV Project Team
 # Universidad Autónoma de Entre Ríos (UADER)
 # Bajo la dirección de Guillermo Gerard y Gallo Fabricio David
 #
@@ -15,3 +14,28 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+"""English spoken-word mapping for the circle dictionary."""
+
+import FreeCADGui as Gui
+from .ayuda import ayuda
+
+TraduceToEn = {
+    # Comandos para crear un círculo desde el centro (create)
+    'create':           lambda: Gui.runCommand('Sketcher_CreateCircle', 0),
+    'circle':           lambda: Gui.runCommand('Sketcher_CreateCircle', 0),
+    'center circle':    lambda: Gui.runCommand('Sketcher_CreateCircle', 0),
+    'circle by center': lambda: Gui.runCommand('Sketcher_CreateCircle', 0),
+
+    # Comandos para crear un círculo mediante 3 puntos (3point)
+    'three point':      lambda: Gui.runCommand('Sketcher_Create3PointCircle', 0),
+    'three points':     lambda: Gui.runCommand('Sketcher_Create3PointCircle', 0),
+    '3 point':          lambda: Gui.runCommand('Sketcher_Create3PointCircle', 0),
+    '3 points':         lambda: Gui.runCommand('Sketcher_Create3PointCircle', 0),
+    'circle by 3 points': lambda: Gui.runCommand('Sketcher_Create3PointCircle', 0),
+
+    # Comandos de ayuda integrados
+    'help':             ayuda,
+    'commands':         ayuda,
+    'options':          ayuda
+}
