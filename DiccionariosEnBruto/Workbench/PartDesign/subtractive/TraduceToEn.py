ea397608010a306a415c079ff867c0748379696b
Alexander Borda <165971680+AlexanderGBordaa@users.noreply.github.com>
2026-06-09 16:31:44 -0300

    Implement English mappings for subtractive commands
    Added English mappings for subtractive commands in PartDesign.
    
    Signed-off-by: Alexander Borda <165971680+AlexanderGBordaa@users.noreply.github.com>

--- DiccionariosEnBruto/Workbench/PartDesign/subtractive/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/PartDesign/subtractive/TraduceToEn.py
@@ -15,3 +15,67 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+"""English spoken-word mapping for PartDesign subtractive commands."""
+
+from .subtractive import subtractive
+from .ayuda import ayuda
+
+TraduceToEn = {
+    # Pocket
+    "pocket": subtractive["pocket"],
+    "cut": subtractive["pocket"],
+
+    # Groove
+    "groove": subtractive["groove"],
+    "slot": subtractive["groove"],
+
+    # Hole
+    "hole": subtractive["hole"],
+    "drill": subtractive["hole"],
+
+    # Subtractive primitives
+    "subtractivebox": subtractive["subtractivebox"],
+    "cutbox": subtractive["subtractivebox"],
+
+    "subtractivecone": subtractive["subtractivecone"],
+    "cutcone": subtractive["subtractivecone"],
+
+    "subtractivecylinder": subtractive["subtractivecylinder"],
+    "cutcylinder": subtractive["subtractivecylinder"],
+
+    "subtractiveellipsoid": subtractive["subtractiveellipsoid"],
+    "cutellipsoid": subtractive["subtractiveellipsoid"],
+
+    "subtractivehelix": subtractive["subtractivehelix"],
+    "cuthelix": subtractive["subtractivehelix"],
+
+    "subtractiveloft": subtractive["subtractiveloft"],
+    "cutloft": subtractive["subtractiveloft"],
+
+    "subtractivepipe": subtractive["subtractivepipe"],
+    "cutpipe": subtractive["subtractivepipe"],
+
+    "subtractiveprism": subtractive["subtractiveprism"],
+    "cutprism": subtractive["subtractiveprism"],
+
+    "subtractivesphere": subtractive["subtractivesphere"],
+    "cutsphere": subtractive["subtractivesphere"],
+
+    "subtractivetorus": subtractive["subtractivetorus"],
+    "cuttorus": subtractive["subtractivetorus"],
+
+    "subtractivewedge": subtractive["subtractivewedge"],
+    "cutwedge": subtractive["subtractivewedge"],
+
+    # Boolean
+    "boolean": subtractive["boolean"],
+    "booleanoperation": subtractive["boolean"],
+    "booleanop": subtractive["boolean"],
+
+    # Help
+    "help": ayuda,
+    "manual": ayuda,
+    "support": ayuda,
+    "documentation": ayuda,
+}
