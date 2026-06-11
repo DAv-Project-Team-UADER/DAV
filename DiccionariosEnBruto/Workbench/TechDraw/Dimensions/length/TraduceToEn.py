820294c65d49ed47d7ee64e54f30d0e1a753a4e1
Valentina Schaab <valentischaab@gmail.com>
2026-06-11 11:14:56 -0300

    Update length TraduceToEn.py
    Signed-off-by: Valentina Schaab <valentischaab@gmail.com>

--- DiccionariosEnBruto/Workbench/TechDraw/Dimensions/length/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/TechDraw/Dimensions/length/TraduceToEn.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2026 El Equipo del Proyecto DAV
-# Copyright (C) 2026 The DAV Project Team
 # Universidad Autónoma de Entre Ríos (UADER)
 # Bajo la dirección de Guillermo Gerard y Gallo Fabricio David
 #
@@ -15,3 +14,17 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import FreeCAD as App
+import FreeCADGui as Gui
+from .ayuda import ayuda
+from .length import _create_length
+
+TraduceToEn = {
+    'length':            lambda: _create_length(),
+    'length dimension':  lambda: _create_length(),  # synonym
+    'distance':          lambda: _create_length(),  # synonym
+    'measure':           lambda: _create_length(),  # synonym
+    'help':              ayuda,
+    'assistance':        ayuda,  # synonym
+}
