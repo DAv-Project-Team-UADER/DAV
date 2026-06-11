eb7e21a89a5a8468527561dae059677534aa66ec
Valentina Schaab <valentischaab@gmail.com>
2026-06-11 11:10:26 -0300

    Update extent TraduceToEn.py
    Signed-off-by: Valentina Schaab <valentischaab@gmail.com>

--- DiccionariosEnBruto/Workbench/TechDraw/Dimensions/extent/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/TechDraw/Dimensions/extent/TraduceToEn.py
@@ -15,3 +15,17 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import FreeCAD as App
+import FreeCADGui as Gui
+from .ayuda import ayuda
+from .extent import _create_extent
+
+TraduceToEn = {
+    'extent':            lambda: _create_extent(),
+    'extent dimension':  lambda: _create_extent(),  # synonym
+    'span':              lambda: _create_extent(),  # synonym
+    'total length':      lambda: _create_extent(),  # synonym
+    'help':              ayuda,
+    'assistance':        ayuda,  # synonym
+}
