3f7d410a9270091178d581235c05193b7b555d0c
Valentina Schaab <valentischaab@gmail.com>
2026-06-12 16:57:57 -0300

    Update TraduceToEn.py
    Signed-off-by: Valentina Schaab <valentischaab@gmail.com>

--- DiccionariosEnBruto/Workbench/TechDraw/Dimensions/angle/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/TechDraw/Dimensions/angle/TraduceToEn.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2026 El Equipo del Proyecto DAV
-# Copyright (C) 2026 The DAV Project Team
 # Universidad Autónoma de Entre Ríos (UADER)
 # Bajo la dirección de Guillermo Gerard y Gallo Fabricio David
 #
@@ -16,19 +15,20 @@
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import FreeCAD as App
-import FreeCADGui as Gui
+from .angle import angle
 from .ayuda import ayuda
-from .angle import _create_angle, _create_angle_3pt
 
 TraduceToEn = {
-    'angle':              lambda: _create_angle(),
-    'angular':            lambda: _create_angle(),        # synonym
-    'angle dimension':    lambda: _create_angle(),        # synonym
-    'points':             lambda: _create_angle_3pt(),
-    'three points':       lambda: _create_angle_3pt(),    # synonym
-    'angle three points': lambda: _create_angle_3pt(),    # synonym
-    '3 point angle':      lambda: _create_angle_3pt(),    # synonym
-    'help':               ayuda,
-    'assistance':         ayuda,  # synonym
+    # angle
+    "angle":              angle["angle"],
+    "angular":            angle["angle"],         # synonym
+    "angle dimension":    angle["angle"],         # synonym
+    # points
+    "points":             angle["points"],
+    "three points":       angle["points"],        # synonym
+    "angle three points": angle["points"],        # synonym
+    "3 point angle":      angle["points"],        # synonym
+    # help
+    "help":               ayuda,
+    "assistance":         ayuda,                  # synonym
 }
