8739d1f0b6adfb4641246c51c6c98ad2885ff8cb
Valentina Schaab <valentischaab@gmail.com>
2026-06-15 09:17:56 -0300

    Update TraduceToEs.py
    Signed-off-by: Valentina Schaab <valentischaab@gmail.com>

--- DiccionariosEnBruto/Workbench/TechDraw/Dimensions/horizontal/TraduceToEs.py
+++ DiccionariosEnBruto/Workbench/TechDraw/Dimensions/horizontal/TraduceToEs.py
@@ -15,3 +15,16 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from .horizontal import horizontal
+
+TraduceToEs = {
+    # horizontal
+    "horizontal":            horizontal["horizontal"],
+    "dimension horizontal":  horizontal["horizontal"],  # sinonimo
+    "ancho":                 horizontal["horizontal"],  # sinonimo
+    # help
+    "ayuda":                 horizontal["help"],
+    "info":                  horizontal["help"],        # sinonimo
+    "opciones":              horizontal["help"],        # sinonimo
+}
