761aad3727f3d3ef4742e13731230479b372df10
Valentina Schaab <valentischaab@gmail.com>
2026-06-15 09:15:40 -0300

    Update TraduceToEs.py
    Signed-off-by: Valentina Schaab <valentischaab@gmail.com>

--- DiccionariosEnBruto/Workbench/TechDraw/Dimensions/extent/TraduceToEs.py
+++ DiccionariosEnBruto/Workbench/TechDraw/Dimensions/extent/TraduceToEs.py
@@ -15,3 +15,16 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from .extent import extent
+
+TraduceToEs = {
+    # extent
+    "extension":          extent["extent"],
+    "dimension extension": extent["extent"],  # sinonimo
+    "longitud total":     extent["extent"],   # sinonimo
+    # help
+    "ayuda":              extent["help"],
+    "info":               extent["help"],     # sinonimo
+    "opciones":           extent["help"],     # sinonimo
+}
