0efa0d58e5d916588f0cc29f629618645dab763a
DevisAriel <bernhardtd87@gmail.com>
2026-06-17 01:48:02 -0300

    Update TraduceToEn.py with new translations
    Added translations for various features and help.
    
    Signed-off-by: DevisAriel <bernhardtd87@gmail.com>

--- DiccionariosEnBruto/Workbench/PartDesign/manage/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/PartDesign/manage/TraduceToEn.py
@@ -15,3 +15,20 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import manage as manage
+import ayuda as ayuda
+
+TraduceToEn = {
+    "movefeature": manage["movefeature"],
+    "move feature": manage["movefeature"],
+    "movefeatureintree": manage["movefeatureintree"],
+    "move feature in tree": manage["movefeatureintree"],
+    "movetip": manage["movetip"],
+    "move tip": manage["movetip"],
+    "preferences": manage["preferences"],
+    "settings": manage["preferences"],
+    "wizardshaft": manage["wizardshaft"],
+    "wizard shaft": manage["wizardshaft"],
+    "help": ayuda,
+}
