e02872f43e70830e7e85598e976dfb225850b9ad
DevisAriel <bernhardtd87@gmail.com>
2026-06-17 01:51:29 -0300

    Add comments for clarity in TraduceToEn.py
    Signed-off-by: DevisAriel <bernhardtd87@gmail.com>

--- DiccionariosEnBruto/Workbench/PartDesign/manage/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/PartDesign/manage/TraduceToEn.py
@@ -20,15 +20,21 @@ import manage as manage
 import ayuda as ayuda
 
 TraduceToEn = {
+    # MoveFeature
     "movefeature": manage["movefeature"],
     "move feature": manage["movefeature"],
+    # MoveFeatureInTree
     "movefeatureintree": manage["movefeatureintree"],
     "move feature in tree": manage["movefeatureintree"],
+    # MoveTip
     "movetip": manage["movetip"],
     "move tip": manage["movetip"],
+    # Preferences
     "preferences": manage["preferences"],
     "settings": manage["preferences"],
+    # WizardShaft
     "wizardshaft": manage["wizardshaft"],
     "wizard shaft": manage["wizardshaft"],
+    
     "help": ayuda,
 }
