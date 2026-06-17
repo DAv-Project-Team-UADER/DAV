619978fabecff8136eda49b940022db41bc3348d
DevisAriel <bernhardtd87@gmail.com>
2026-06-17 01:46:45 -0300

    Add translation dictionary for additive features
    Signed-off-by: DevisAriel <bernhardtd87@gmail.com>

--- DiccionariosEnBruto/Workbench/PartDesign/additive/TraduceToEn.py
+++ DiccionariosEnBruto/Workbench/PartDesign/additive/TraduceToEn.py
@@ -15,3 +15,35 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+import additive as additive
+import ayuda as ayuda
+
+traduceToEn = {
+    "pad": additive["pad"],
+    "pad feature": additive["pad"],
+    "revolution": additive["revolution"],
+    "revolution feature": additive["revolution"],
+    "additive helix": additive["additivehelix"],
+    "Coil feature": additive["additivehelix"],
+    "additive loft": additive["additiveloft"],
+    "loft feature": additive["additiveloft"],
+    "additive pipe": additive["additivepipe"],
+    "pipe feature": additive["additivepipe"],
+    "additive box": additive["additivebox"],
+    "Additive solid box": additive["additivebox"],
+    "additive cone": additive["additivecone"],
+    "cone feature": additive["additivecone"],
+    "additive cylinder": additive["additivecylinder"],
+    "cylinder feature": additive["additivecylinder"],
+    "additive ellipsoid": additive["additiveellipsoid"],
+    "ellipsoid feature": additive["additiveellipsoid"],
+    "additive prism": additive["additiveprism"],
+    "prism feature": additive["additiveprism"],
+    "additive sphere": additive["additivesphere"],
+    "sphere feature": additive["additivesphere"],
+    "additive torus": additive["additivetorus"],
+    "torus feature": additive["additivetorus"],
+    "additive wedge": additive["additivewedge"],
+    "wedge feature": additive["additivewedge"],
+    "help": ayuda,
+}
