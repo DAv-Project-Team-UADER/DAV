888e10259e75d50a5545a6b687b9aaba77cac2da
Abrilpoggio <163590084+Abrilpoggio@users.noreply.github.com>
2026-06-22 14:59:39 -0300

    Add Portuguese translations for toolbar items
    Signed-off-by: Abrilpoggio <163590084+Abrilpoggio@users.noreply.github.com>

--- DiccionariosEnBruto/StdView/Toolbars/TraduceToPt.py
+++ DiccionariosEnBruto/StdView/Toolbars/TraduceToPt.py
@@ -15,3 +15,55 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+from .toolbars import toolbars
+from .ayuda import ayuda
+
+TraduceToPt = {
+
+    # Área de transferência
+    "área de transferência": toolbars["clipboard"],
+    "area de transferencia": toolbars["clipboard"],
+    "barra da área de transferência": toolbars["clipboard"],
+    "copiar": toolbars["clipboard"],
+    "colar": toolbars["clipboard"],
+    "recortar": toolbars["clipboard"],
+    # Editar
+    "editar": toolbars["edit"],
+    "edição": toolbars["edit"],
+    "edicao": toolbars["edit"],
+    "barra de edição": toolbars["edit"],
+    "barra de edicao": toolbars["edit"],
+    # Arquivo
+    "arquivo": toolbars["file"],
+    "barra de arquivos": toolbars["file"],
+    # Barra de ajuda
+    "barra de ajuda": toolbars["toolbarshelp"],
+    "ajuda da barra": toolbars["toolbarshelp"],
+    # Vistas
+    "vistas": toolbars["views"],
+    "vistas individuais": toolbars["views"],
+    "barra de vistas": toolbars["views"],
+    # Bloquear barras
+    "bloquear": toolbars["lock"],
+    "bloquear barra": toolbars["lock"],
+    "bloquear barras": toolbars["lock"],
+    "desbloquear barra": toolbars["lock"],
+    "desbloquear barras": toolbars["lock"],
+    # Macro
+    "macro": toolbars["macro"],
+    "macros": toolbars["macro"],
+    "barra de macros": toolbars["macro"],
+    # Estrutura
+    "estrutura": toolbars["structure"],
+    "barra de estrutura": toolbars["structure"],
+    # Vista
+    "vista": toolbars["view"],
+    "barra de vista": toolbars["view"],
+    # Bancada
+    "bancada": toolbars["workbench"],
+    "barra da bancada": toolbars["workbench"],
+    "workbench": toolbars["workbench"],
+    # Ajuda
+    "ajuda": ayuda,
+}
