41a32305384d59111f03fbc116a6066227d2ab27
benjaklim <benjaklimczuk@gmail.com>
2026-07-14 14:36:37 -0300

    Feat: Completadas traducciones de Visibilidad (En, Es, Pt) y sinónimos

--- DiccionariosEnBruto/StdView/Visibility/TraduceToEs.py
+++ DiccionariosEnBruto/StdView/Visibility/TraduceToEs.py
@@ -15,3 +15,122 @@
 # Deberías haber recibido una copia de la Licencia Pública General GNU
 # junto con este programa. Si no es así, consulte <http://www.gnu.org/licenses/>.
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Spanish spoken-word mapping for the DAV StdView/Visibility dictionary folder."""
+
+from .Visibility import visibility
+
+TraduceToEs = {
+    # Claves principales
+    "hideobjects":      visibility["hideobjects"],
+    "hide":             visibility["hide"],
+    "alllinks":         visibility["alllinks"],
+    "linked":           visibility["linked"],
+    "linkedfinal":      visibility["linkedfinal"],
+    "selback":          visibility["selback"],
+    "boundingbox":      visibility["boundingbox"],
+    "selforward":       visibility["selforward"],
+    "selectvisible":    visibility["selectvisible"],
+    "showobjects":      visibility["showobjects"],
+    "show":             visibility["show"],
+    "toggleall":        visibility["toggleall"],
+    "selectability":    visibility["selectability"],
+    "transparency":     visibility["transparency"],
+    "toggle":           visibility["toggle"],
+    "aligntoselection": visibility["aligntoselection"],
+
+    # Sinónimos en español
+    "ocultar objetos":      visibility["hideobjects"],
+    "ocultar todo":         visibility["hideobjects"],
+    "ocultar todos":        visibility["hideobjects"],
+    "esconder objetos":     visibility["hideobjects"],
+    "esconder todo":        visibility["hideobjects"],
+
+    "ocultar":              visibility["hide"],
+    "esconder":             visibility["hide"],
+    "ocultar seleccion":    visibility["hide"],
+    "ocultar selección":    visibility["hide"],
+    "esconder seleccion":   visibility["hide"],
+    "esconder selección":   visibility["hide"],
+
+    "todos los links":      visibility["alllinks"],
+    "todos los enlaces":    visibility["alllinks"],
+    "seleccionar todos los links": visibility["alllinks"],
+    "seleccionar todos los enlaces": visibility["alllinks"],
+
+    "vinculado":            visibility["linked"],
+    "objeto vinculado":     visibility["linked"],
+    "enlazado":             visibility["linked"],
+    "ir al vinculo":        visibility["linked"],
+    "ir al vínculo":        visibility["linked"],
+
+    "vinculo final":        visibility["linkedfinal"],
+    "vínculo final":        visibility["linkedfinal"],
+    "enlace final":         visibility["linkedfinal"],
+    "vinculado final":      visibility["linkedfinal"],
+
+    "atras seleccion":      visibility["selback"],
+    "atrás selección":      visibility["selback"],
+    "retroceder seleccion":  visibility["selback"],
+    "retroceder selección":  visibility["selback"],
+    "volver seleccion":     visibility["selback"],
+    "volver selección":     visibility["selback"],
+
+    "caja de colision":     visibility["boundingbox"],
+    "caja de colisión":     visibility["boundingbox"],
+    "caja delimitadora":    visibility["boundingbox"],
+    "caja limite":          visibility["boundingbox"],
+    "caja límite":          visibility["boundingbox"],
+    "bounding box":         visibility["boundingbox"],
+
+    "adelante seleccion":   visibility["selforward"],
+    "adelante selección":   visibility["selforward"],
+    "avanzar seleccion":    visibility["selforward"],
+    "avanzar selección":    visibility["selforward"],
+
+    "seleccionar visibles": visibility["selectvisible"],
+    "seleccionar visible":  visibility["selectvisible"],
+    "seleccionar objetos visibles": visibility["selectvisible"],
+
+    "mostrar objetos":      visibility["showobjects"],
+    "mostrar todo":         visibility["showobjects"],
+    "mostrar todos":        visibility["showobjects"],
+    "hacer visible todo":   visibility["showobjects"],
+    "revelar objetos":      visibility["showobjects"],
+
+    "mostrar":              visibility["show"],
+    "revelar":              visibility["show"],
+    "mostrar seleccion":    visibility["show"],
+    "mostrar selección":    visibility["show"],
+
+    "alternar todo":        visibility["toggleall"],
+    "conmutar todo":        visibility["toggleall"],
+    "toggle todo":          visibility["toggleall"],
+
+    "seleccionabilidad":    visibility["selectability"],
+    "alternar seleccionabilidad": visibility["selectability"],
+    "alternar selecciónabilidad": visibility["selectability"],
+    "permitir seleccion":   visibility["selectability"],
+    "permitir selección":   visibility["selectability"],
+
+    "transparencia":        visibility["transparency"],
+    "alternar transparencia": visibility["transparency"],
+    "transparente":         visibility["transparency"],
+
+    "alternar":             visibility["toggle"],
+    "alternar visibilidad": visibility["toggle"],
+    "conmutar visibilidad": visibility["toggle"],
+
+    "alinear a seleccion":  visibility["aligntoselection"],
+    "alinear a selección":  visibility["aligntoselection"],
+    "alinear con seleccion": visibility["aligntoselection"],
+    "alinear con selección": visibility["aligntoselection"],
+    "perpendicular a la seleccion": visibility["aligntoselection"],
+    "perpendicular a la selección": visibility["aligntoselection"],
+
+    # Ayuda
+    "ayuda":                visibility["help"],
+    "informacion":          visibility["help"],
+    "información":          visibility["help"],
+    "opciones":             visibility["help"],
+}
