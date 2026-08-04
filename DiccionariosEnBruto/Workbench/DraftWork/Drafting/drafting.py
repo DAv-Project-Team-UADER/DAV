2d5da2ef4842dcd35eb9209ba7572f335c5abdae
Alexander Borda <165971680+AlexanderGBordaa@users.noreply.github.com>
2026-08-04 17:08:21 -0300

    Refactor wire command into separate function
    Signed-off-by: Alexander Borda <165971680+AlexanderGBordaa@users.noreply.github.com>

--- DiccionariosEnBruto/Workbench/DraftWork/Drafting/drafting.py
+++ DiccionariosEnBruto/Workbench/DraftWork/Drafting/drafting.py
@@ -1,7 +1,18 @@
+import FreeCAD as App
 import FreeCADGui as Gui
+
+from createobjects import CreateObjects
 from .ayuda import ayuda
 
+
+def wire():
+    Gui.runCommand("Draft_Wire", 0)
+
+    obj = App.ActiveDocument.ActiveObject
+    CreateObjects(Is3D=False).Execute(obj)
+
+
 drafting = {
-    'wire': lambda: Gui.runCommand('Draft_Wire', 0),
-    'help': ayuda
+    "wire": wire,
+    "help": ayuda,
 }
