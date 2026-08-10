4fe1cb6b9a82b2612a24736b714c2a3ba25977b5
Camila Viñeg <vinegcamila27@gmail.com>
2026-08-10 16:25:58 -0300

    modificaciones

--- Dav/scr/ComponentesDAV/InterfazDAV/run_interfaz.bat
+++ Dav/scr/ComponentesDAV/InterfazDAV/run_interfaz.bat
@@ -1,4 +1,4 @@
 @echo off
 set DAV_PASSIVE_HISTORY_VIEWER=1
-cd /d "C:\Trabajos Facu\PET\DAV\DAV\Dav\scr\ComponentesDAV\InterfazDAV"
-start "" "C:\Trabajos Facu\PET\DAV\DAV\Dav\scr\ComponentesDAV\IntegracionGUI\GUIFreeCad\.venv\Scripts\pythonw.exe" "C:\Trabajos Facu\PET\DAV\DAV\Dav\scr\ComponentesDAV\InterfazDAV\main.py"
+cd /d "C:\Repos\PET-DAV-2026\DAV\Dav\scr\ComponentesDAV\InterfazDAV"
+start "" "C:\Repos\PET-DAV-2026\DAV\Dav\scr\ComponentesDAV\IntegracionGUI\GUIFreeCad\.venv\Scripts\pythonw.exe" "C:\Repos\PET-DAV-2026\DAV\Dav\scr\ComponentesDAV\InterfazDAV\main.py"
