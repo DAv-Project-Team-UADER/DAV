01a68beac88c56217231cc098fa09e9d99b20dc2
TadeoRochas <tadeorochas05@gmail.com>
2026-08-28 18:18:24 -0300

    Delete Dav/scr/ComponentesDAV/InterfazDAV/run_interfaz.bat
    Signed-off-by: TadeoRochas <tadeorochas05@gmail.com>

--- Dav/scr/ComponentesDAV/InterfazDAV/run_interfaz.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\poggi\Desktop\DAV\Dav\scr\ComponentesDAV\InterfazDAV"
-start "" "C:\Users\poggi\Desktop\DAV\Dav\scr\ComponentesDAV\IntegracionGUI\GUIFreeCad\.venv\Scripts\pythonw.exe" "C:\Users\poggi\Desktop\DAV\Dav\scr\ComponentesDAV\InterfazDAV\main.py"
