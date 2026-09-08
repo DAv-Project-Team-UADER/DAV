3ec02b6ad2df52fe5ad1e55966ec89dec2f57f2c
Lucas Tabacchi <164110892+LucasTabacchi@users.noreply.github.com>
2026-09-08 17:29:33 -0300

    Add global panel commands to TraduceToPt.py
    Added global commands for panel control in Portuguese.
    
    Signed-off-by: Lucas Tabacchi <164110892+LucasTabacchi@users.noreply.github.com>

--- Dav/dic/StdView/Appearance/TraduceToPt.py
+++ Dav/dic/StdView/Appearance/TraduceToPt.py
@@ -19,6 +19,7 @@
 from .Appearance import appearance
 from .ayuda import ayuda
 from .Appearance import appearance as Appearance
+from StdView.Panels.Panels import Panels
 
 TraduceToPt = {
 
@@ -46,4 +47,12 @@ TraduceToPt = {
     'ajuda':                Appearance['help'],
     'informação':           Appearance['help'],
     'opções':               Appearance['help'],
+
+    # Comandos Globales
+    "maximizar": Panels['panel'],
+    "minimizar": Panels['panel'],
+    "reduzir": Panels['panel'],
+    "aumentar": Panels['panel'],
+    "diminuir": Panels['panel'],
+    "ampliar": Panels['panel'],
 }
